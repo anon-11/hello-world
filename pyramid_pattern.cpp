67cb8314bd8eda884b987dbbecf16324c65a92e7
anon-11 <114279690+anon-11@users.noreply.github.com>
2024-02-16 11:06:46 +0530

    Update pyramid_pattern.cpp
    removed all of the commented lines (used for doxygen)  :)

--- pyramid_pattern.cpp
+++ pyramid_pattern.cpp
@@ -1,27 +1,13 @@
-/**
- * @file pyramid_pattern.cpp
- * @brief C++ Program to print right half pyramid pattern using for loop.
- */
+
 
 #include <iostream>
 
-/**
- * @brief Main function to print the right half pyramid pattern.
- * @return 0 on successful execution.
- */
 int main()
 {
     int rows = 5;
 
-    // First for loop is used to identify number of rows
     for (int i = 1; i <= rows; i++) {
-
-        // Second for loop is used to identify number of
-        // columns and here the values will be changed
-        // according to the first for loop
         for (int j = 1; j <= i; j++) {
-
-            // Printing the required pattern
             std::cout << "* ";
         }
         std::cout << "\n";
